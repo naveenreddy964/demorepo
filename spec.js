6093753bbe5793dc701d65f04d23741b32ec630e
Naveen0417 <35036512+Naveen0417@users.noreply.github.com>
2020-11-05 18:21:16 +0530

    Update spec.js

--- spec.js
+++ spec.js
@@ -1,6 +1,6 @@
 //it is like a Test file means in java (class file)
 describe('Protractor baby steps',function() { //param-1(test suite name)
-    // param-2(function(function will have all tests(it blocks)
+    
 
 it('open the apllication',function(){  //write ur protractor test scrpit or case here
 
